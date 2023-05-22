25ce5c921fd3f4cd7351d1a72dd367ad6090175e
Jitendra253 <107558211+Jitendra253@users.noreply.github.com>
2023-05-22 21:39:48 +0530

    deployment error fixed

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors())
 app.use(express.json());
 app.use(morgan("dev"));
 //link react application path 
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, '/client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -43,7 +43,7 @@ app.use("/api/v1/product", productRoutes)
 
 //display our react file 
 app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'))
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
 })
 
 //PORT
