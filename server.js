888cc164930ed76d87a2d39c62cb55b8b69d5da0
Jitendra253 <107558211+Jitendra253@users.noreply.github.com>
2023-05-22 22:11:17 +0530

    deploy error fix

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
 import path from 'path'
+import { dirname } from 'path';
 import { fileURLToPath } from "url";
 
 //configure env
@@ -21,15 +22,17 @@ const app = express();
 
 
 //esmodule fix
+// Get current module file path
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = fileURLToPath(import.meta.url)
+// Get current module directory path
+const __dirname = dirname(__filename);
 
 //middleware
 app.use(cors())
 app.use(express.json());
 app.use(morgan("dev"));
-//link react application path 
-app.use(express.static(path.join(__dirname, '/client/build')))
+// Link react application path
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -41,10 +44,10 @@ app.use("/api/v1/product", productRoutes)
 //   res.send("<h1>Welcome to Book Selling App: BookMagnet</h1>");
 // });
 
-//display our react file 
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'))
-})
+// Display our react file
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 //PORT
 const PORT = process.env.PORT || 8080;
